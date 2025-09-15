6e5df33adc58c065fa2c03c09235c8664a592ff6
Lugia11 <noahgunnarsson2011@hotmail.com>
2025-09-15 19:20:05 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+#The imports
+import keyboard
+#Checks for W
+def on_keydown(event):
+    
+    if event.name == "w":
+        output_div = document.querySelector("#WArea")
+        output_div.innerHTML = "W was pressed"
+keyboard.on_press(on_keydown)
+#Checks for S
+def on_keydown(event):
+    if event.name == "s":
+        output_div = document.querySelector("#SArea")
+        output_div.innerHTML = "S was pressed"
+keyboard.on_press(on_keydown)
+
+
+#keep it running
+keyboard.wait()
